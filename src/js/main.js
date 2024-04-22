9bd61f7e1ea1561dfb947ab612c5ca0cb96a196d
Haukur Hlöðversson <haukur@haukurh.is>
2024-04-22 08:42:53 +0000

    Run prettier on project

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,51 @@
 const registerServiceWorker = async () => {
-    if ('serviceWorker' in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register(
-                '/sw.js',
-                {
-                    scope: '/',
-                }
-            );
-            if (registration.installing) {
-                console.log('Service worker installing');
-            } else if (registration.waiting) {
-                console.log('Service worker installed');
-            } else if (registration.active) {
-                console.log('Service worker active');
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js', {
+        scope: '/',
+      });
+      if (registration.installing) {
+        console.log('Service worker installing');
+      } else if (registration.waiting) {
+        console.log('Service worker installed');
+      } else if (registration.active) {
+        console.log('Service worker active');
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
     }
+  }
 };
 
 const unregisterServiceWorker = async () => {
-    return new Promise((resolve, reject) => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.getRegistration('/')
-                .then((registration) => {
-                    if (registration) {
-                        registration.unregister()
-                            .then((result) => {
-                                if (result) {
-                                    console.log('Service worker unregistered!');
-                                    resolve(result);
-                                } else {
-                                    console.warn('Unable to unregister service worker');
-                                    reject(result);
-                                }
-                            })
-                            .catch((e) => {
-                                console.error('Error when unregistering service worker', {
-                                    error: e,
-                                });
-                                reject(e);
-                            });
-                    } else {
-                        console.warn('No service worker registration available!');
-                        reject();
-                    }
-                });
+  return new Promise((resolve, reject) => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistration('/').then((registration) => {
+        if (registration) {
+          registration
+            .unregister()
+            .then((result) => {
+              if (result) {
+                console.log('Service worker unregistered!');
+                resolve(result);
+              } else {
+                console.warn('Unable to unregister service worker');
+                reject(result);
+              }
+            })
+            .catch((e) => {
+              console.error('Error when unregistering service worker', {
+                error: e,
+              });
+              reject(e);
+            });
+        } else {
+          console.warn('No service worker registration available!');
+          reject();
         }
-    });
+      });
+    }
+  });
 };
 
 registerServiceWorker();
@@ -56,46 +53,47 @@ registerServiceWorker();
 const popularList = ['AUD', 'USD', 'GBP', 'SEK', 'EUR', 'THB'];
 
 const db = {
-    get: (key) => JSON.parse(window.localStorage.getItem(key)),
-    set: (key, value) => window.localStorage.setItem(key, JSON.stringify(value)),
-    remove: (key) => window.localStorage.removeItem(key),
-    clear: () => window.localStorage.clear(),
-    keys: {
-        USER_SELECTED_CURRENCIES: 'userSelectedCurrencies',
-        CURRENCIES: 'currencies',
-        CURRENT: 'current',
-        UPDATED_AT: 'updatedAt',
-        NUMBER_FORMAT: 'numberFormat',
-    },
+  get: (key) => JSON.parse(window.localStorage.getItem(key)),
+  set: (key, value) => window.localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => window.localStorage.removeItem(key),
+  clear: () => window.localStorage.clear(),
+  keys: {
+    USER_SELECTED_CURRENCIES: 'userSelectedCurrencies',
+    CURRENCIES: 'currencies',
+    CURRENT: 'current',
+    UPDATED_AT: 'updatedAt',
+    NUMBER_FORMAT: 'numberFormat',
+  },
 };
 
 const getUserSelectedCurrencies = () => {
-    const key = db.keys.USER_SELECTED_CURRENCIES;
-    const selected = db.get(key);
-    if (!selected) {
-        db.set(key, popularList);
-        return popularList;
-    }
-    if (!Array.isArray(selected)) {
-        db.remove(key);
-        return getUserSelectedCurrencies();
-    }
-    return selected;
+  const key = db.keys.USER_SELECTED_CURRENCIES;
+  const selected = db.get(key);
+  if (!selected) {
+    db.set(key, popularList);
+    return popularList;
+  }
+  if (!Array.isArray(selected)) {
+    db.remove(key);
+    return getUserSelectedCurrencies();
+  }
+  return selected;
 };
 
 const getUserSelectedCurrency = () => {
-    const userSelectedCurrencies = getUserSelectedCurrencies();
-    const current = db.get(db.keys.CURRENT);
-    if (!current) {
-        console.log('No currency selected for user, setting default');
-        db.set(db.keys.CURRENT, userSelectedCurrencies[0]);
-        return userSelectedCurrencies[0];
-    }
-    if (!userSelectedCurrencies.includes(current)) {
-        console.log('Selected currency not available, resetting to first as default');
-        db.set(db.keys.CURRENT, userSelectedCurrencies[0]);
-        return userSelectedCurrencies[0];
-    }
-    return current;
+  const userSelectedCurrencies = getUserSelectedCurrencies();
+  const current = db.get(db.keys.CURRENT);
+  if (!current) {
+    console.log('No currency selected for user, setting default');
+    db.set(db.keys.CURRENT, userSelectedCurrencies[0]);
+    return userSelectedCurrencies[0];
+  }
+  if (!userSelectedCurrencies.includes(current)) {
+    console.log(
+      'Selected currency not available, resetting to first as default',
+    );
+    db.set(db.keys.CURRENT, userSelectedCurrencies[0]);
+    return userSelectedCurrencies[0];
+  }
+  return current;
 };
-
--- src/sw.js
+++ src/sw.js
@@ -1,88 +1,87 @@
 const version = 'v0.0.11';
 
 const addResourcesToCache = async (resources) => {
-    const cache = await caches.open(version);
-    await cache.addAll(resources);
+  const cache = await caches.open(version);
+  await cache.addAll(resources);
 };
 
 const putInCache = async (request, response) => {
-    const cache = await caches.open(version);
-    await cache.put(request, response);
+  const cache = await caches.open(version);
+  await cache.put(request, response);
 };
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-    // First try to get the resource from the cache
-    const cache = await caches.open(version);
-    const responseFromCache = await cache.match(request, { ignoreSearch: true });
+  // First try to get the resource from the cache
+  const cache = await caches.open(version);
+  const responseFromCache = await cache.match(request, { ignoreSearch: true });
 
-    if (responseFromCache && request.headers.get('cache-control') !== 'no-cache') {
-      const expiresHeader = responseFromCache.headers.get('expires');
-      if (expiresHeader === null) {
-        // console.log('No expire header, serving from cache');
-        return responseFromCache;
-      }
-
-      if ((new Date()) < (new Date(expiresHeader))) {
-        // console.log('Cache is still valid');
-        return responseFromCache;
-      }
-      console.log('Cache has expired fetching, getting from network');
+  if (
+    responseFromCache &&
+    request.headers.get('cache-control') !== 'no-cache'
+  ) {
+    const expiresHeader = responseFromCache.headers.get('expires');
+    if (expiresHeader === null) {
+      // console.log('No expire header, serving from cache');
+      return responseFromCache;
     }
 
-    // Next try to use the preloaded response, if it's there
-    const preloadResponse = await preloadResponsePromise;
-    if (preloadResponse) {
-        console.info('using preload response');
-        putInCache(request, preloadResponse.clone());
-        return preloadResponse;
+    if (new Date() < new Date(expiresHeader)) {
+      // console.log('Cache is still valid');
+      return responseFromCache;
     }
+    console.log('Cache has expired fetching, getting from network');
+  }
 
-    // Next try to get the resource from the network
-    try {
-        const responseFromNetwork = await fetch(request);
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        putInCache(request, responseFromNetwork.clone());
-        return responseFromNetwork;
-    } catch (error) {
-        if (responseFromCache) {
-            console.warn('Network error unable to fetch resource, using from cache');
-            return responseFromCache;
-        }
-        return new Response('Network error happened', {
-            status: 408,
-            headers: { 'Content-Type': 'text/plain' },
-        });
+  // Next try to use the preloaded response, if it's there
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info('using preload response');
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    if (responseFromCache) {
+      console.warn('Network error unable to fetch resource, using from cache');
+      return responseFromCache;
     }
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
 };
 
 const enableNavigationPreload = async () => {
-    if (self.registration.navigationPreload) {
-        // Enable navigation preloads!
-        await self.registration.navigationPreload.enable();
-    }
+  if (self.registration.navigationPreload) {
+    // Enable navigation preloads!
+    await self.registration.navigationPreload.enable();
+  }
 };
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(enableNavigationPreload());
+  event.waitUntil(enableNavigationPreload());
 });
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        addResourcesToCache([
-            '/',
-            '/index.html',
-            '/currency-rates.json',
-        ])
-    );
+  event.waitUntil(
+    addResourcesToCache(['/', '/index.html', '/currency-rates.json']),
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        cacheFirst({
-            request: event.request,
-            preloadResponsePromise: event.preloadResponse,
-        })
-    );
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+    }),
+  );
 });
